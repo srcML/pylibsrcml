a940a659ca41680b81444dac170e87b5d7aced99
Joshua Behler <Joshua2000B@gmail.com>
2023-04-05 13:57:19 -0400

    Added new test for check_extension

--- testsuite/test_srcml_archive_check_extension.py
+++ testsuite/test_srcml_archive_check_extension.py
@@ -1,11 +1,16 @@
 import pylibsrcml
 
 archive = pylibsrcml.srcMLArchive()
+
 archive.register_file_extension("h","C++")
 
 assert archive.check_extension("a.h") == "C++"
 assert archive.check_extension("a.h.gz") == "C++"
 assert archive.check_extension("a.foo") == None
+
+archive.register_file_extension("foo","C++")
+assert archive.check_extension("a.foo") == "C++"
+assert archive.check_extension("a.foo.gz") == "C++"
 
 try:
     archive.check_extension(0)
